470cf81e1505e719df886725c34a02e99a9e4030
Isaac Dunham <idunham@lavabit.com>
2012-03-09 21:35:42 -0800

    Distutils support

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name = 'sprinkler-precip',
+		version = '0.23',
+		py_modules = ['prf', 'sprinkler'],
+		author = 'Isaac Dunham',
+		author_email = 'idunham@mail.csuchico.edu', 
+		url = 'https://github.com/idunham/sprinkler-precip'
+		)
