7a984daa2edbab5dd9135ccc0307442f5d1a70d3
Isaac Dunham <idunham@lavabit.com>
2012-03-09 20:26:46 -0800

    Implement a real reader (converts to ndarray).  Add stub to properly parse data

--- src/prf.py
+++ src/prf.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 ##############################################################
 # PRF (SPACE precipitation profile) reader.
-# 
+# Provides functions to read a single entry (prfline),
+# convert all entries from f.tell() to the end into a 2-d ndarray
+# (readprf), and parse a given sprinkler's data from string to an
+# array of usable values (NOT YET IMPLEMENTED: parsedata)
+# NOTE: use f.seek(0) before reading more.
 #
+# Doing this as an object with methods and safety checking would 
+# be much better. 
+# Eventually I hope to implement write support as well.
+# This will allow sharing data with SPACE.
 #
-#
+# Calculations based on spacing and other readily accessed 
+# information belong outside this library--alternate formats
+# should be supported.
 ##############################################################
+import numpy as np
 
 def prfline(prf):
 	'''prfline(filehandle prf)
@@ -15,13 +26,39 @@ def prfline(prf):
 	Facility, Date, Time, Sprinkler, Model, Nozzle, PSI, Riser, Flow, 
 	 Spacing, Radius, Arc, Min/Revol, Set Screw, Rec #,
 
-	 Duration, Comment'''
+	 Duration, Comment
+	 Data'''
 	l	=prf.read(500)
 	# Here I must apologize for the mess.
-	# This assigns all values in one pass, and does so in an ugly way
+	# This assigns all values in one pass.
 	larray	=[l[0:20],l[21:28],l[30:34],l[36:55],l[57:62],l[63:82],l[84:92],
 			l[94:98],l[99:106],l[107:109],l[110:113],l[115:119],
 			l[120:124],l[125:129],l[130:140],l[141:145],l[146:186],
 			int(l[187:189]),int(l[190:193]),
 			l[194:499]]
 	return larray
+
+def readprf(File):
+	'''readprf(File) -> 2-d array of strings'''
+	parr=np.zeros((20,0),dtype='|S305')
+	try:
+		line=prf.prfline(File)	#Initialize
+		while line != "":
+			parr=np.append(parr,numpy.array(line).reshape((1,20),0),0)
+			line=prf.prfline(File)
+	except:
+		pass
+	finally:
+		return parr
+
+def parsedata(Type,DataString):
+	'''parsedata(Type,String)->1-d array of numbers
+	There's a 305-byte string containing about 61 readings.
+	This should return those readings.
+	In order to make it meaningful, we need the format selector 
+	(which should be 1-5, if I remember right).
+	The format selector tells whether we need to transform the data.
+	'''
+	pass
+
+
